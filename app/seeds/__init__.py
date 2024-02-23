07d5980bd9aa67bd14374df6a5a08321bbe89ca0
Collin <collinullmann@gmail.com>
2024-02-23 12:46:16 -0500

    changed table seeding order

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,21 +19,21 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
         undo_tracks()
         undo_albums()
+        undo_users()
         undo_likes()
     seed_users()
-    seed_tracks()
     seed_albums()
+    seed_tracks()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
     undo_tracks()
     undo_albums()
+    undo_users()
     undo_likes()
     # Add other undo functions here
