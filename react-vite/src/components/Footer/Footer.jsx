a467642853c854b0f1e1c1400a107b4dc4eebe94
Collin <collinullmann@gmail.com>
2024-02-24 10:34:18 -0500

    added react h5 audio player

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import { useSelector } from 'react-redux'
 import './Footer.css'
 import { useState } from 'react'
 
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
 export function Footer() {
 
 
@@ -15,24 +18,36 @@ export function Footer() {
   //   if (playingTrack) togglePlay()
   // })
 
-  const audio = new Audio(src)
-  function togglePlay() {
-    if (audio.paused) {
-      setIsPlaying(true)
-      audio.play()
-    } else {
-      setIsPlaying(false)
-      audio.pause()
-    }
-  }
+  // const audio = new Audio(src)
+  // function togglePlay() {
+  //   if (audio.paused) {
+  //     setIsPlaying(true)
+  //     audio.play()
+  //   } else {
+  //     setIsPlaying(false)
+  //     audio.pause()
+  //   }
+  // }
 
   return (
-    <div className='track-player'>
-      <div>
-        <h2>{playingTrack ? playingTrack?.title : 'No track selected'}</h2>
-        {playingTrack && <p>{playingTrack?.artistName}</p>}
-      </div>
-      <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+    <div>
+      <AudioPlayer
+      autoPlay
+      src={src}
+      onPlay={e => console.log("onPlay")}
+      // other props here
+      />
     </div>
+
+
+
+
+    // <div className='track-player'>
+    //   <div>
+    //     <h2>{playingTrack ? playingTrack?.title : 'No track selected'}</h2>
+    //     {playingTrack && <p>{playingTrack?.artistName}</p>}
+    //   </div>
+    //   <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+    // </div>
   )
 }
