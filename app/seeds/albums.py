ee7afa14d89d15d7af6c892631a46aee65f1607b
Collin <collinullmann@gmail.com>
2024-02-24 20:53:59 -0500

    okay one last adjustment to the seeders I swear

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -6,12 +6,12 @@ def seed_albums():
     test_image_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/4ddbd8fa75ef47b8814d594183c0bffd.jpg'
     albums = [
 
-        Album(artist_id = 3, title = 'Binary Grooves: The Dj Bytebeat Collection', release_date= date(1997, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-1.jpg'),
+        Album(artist_id = 3, title = 'Binary Grooves: The DJ ByteBeat Collection', release_date= date(1997, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-1.jpg'),
         Album(artist_id = 1, title = 'Metronome', release_date= date(1997, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-6.jpg'),
         Album(artist_id = 2, title = 'Cross', release_date= date(1997, 12, 4), genre = 'EDM', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-4.jpg'),
         Album(artist_id = 2, title = 'Human After All', release_date= date(2005, 12, 4), genre = 'Electronica', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-5.jpg'),
-        Album(artist_id = 3, title = 'Binary Grooves: The Dj Bytebeat Collection 2', release_date= date(1998, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-2.jpg'),
-        Album(artist_id = 3, title = 'Binary Grooves: The Dj Bytebeat Collection 3', release_date= date(1999, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-3.jpg')
+        Album(artist_id = 3, title = 'Binary Grooves: The DJ ByteBeat Collection 2', release_date= date(1998, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-2.jpg'),
+        Album(artist_id = 3, title = 'Binary Grooves: The DJ ByteBeat Collection 3', release_date= date(1999, 12, 4), genre = 'Pop', album_cover_url = 'https://bootcampbeats-tracks.s3.amazonaws.com/seeder-album-3.jpg')
     ]
 
     db.session.add_all(albums)
